6995c30cd7f178b710a2cf4add17da67c270d973
Brenel Once <brenelnico@gmail.com>
2026-08-16 23:23:40 +0800

    Refactor Django admin

--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -6,10 +6,11 @@ class BaseModel(models.Model):
     updated_at = models.DateTimeField(auto_now=True) 
     class Meta: 
         abstract = True 
-class College(BaseModel): 
-    college_name = models.CharField(max_length=150) 
-def __str__(self): 
-    return self.college_name
+class College(BaseModel):
+    college_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.college_name
 class Program(BaseModel):
     prog_name = models.CharField(max_length=150) 
     college = models.ForeignKey(College, on_delete=models.CASCADE) 
