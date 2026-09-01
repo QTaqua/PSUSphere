64dff44fab389fa5e7d9f65a3f34854d2f2d1b65
QTaqua <jabenca05@gmail.com>
2026-09-01 13:56:03 +0800

    edited settings py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -69,12 +69,8 @@ TEMPLATES = [
     },
 ]
 
-STATIC_URL = '/static/'
+
 WSGI_APPLICATION = 'projectsite.wsgi.application'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-) 
 
 
 # Database
@@ -123,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+) 
 
 
 # Email
