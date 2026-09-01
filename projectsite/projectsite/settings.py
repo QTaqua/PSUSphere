fa3e5372a78d752e8ddbf88883695b4b9eb68e20
QTaqua <jabenca05@gmail.com>
2026-09-01 15:13:57 +0800

    testrun

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-51f8&u$5_j9cdc8d3$nb8ky!#1_6qxv%z5d7a3_*4=)t&4dpzl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['qtaqua.pythonanywhere.com', 'localhost', '127.0.0.1']
 
